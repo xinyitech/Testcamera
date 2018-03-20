0d3dfce9fadf7824f3584917a53f13dc6b1aba21
unknown <bangyong.zhang@zhangbangyongpc.xinyi.sz>
2018-03-20 12:12:56 +0800

    first

---
+++ app/src/main/java/com/example/administrator/testCamera/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.administrator.testCamera.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.administrator.testCamera.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_open).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+        });
+    }
+}
